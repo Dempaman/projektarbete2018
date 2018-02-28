be64ebbc9aff7af5dba5279b64e07bd44cfd10b1
sabrinapalm <sabrina_palm@hotmail.com>
2018-02-28 13:46:58 +0100

    added api querystring as comment

--- js/events.js
+++ js/events.js
@@ -3,6 +3,14 @@ document.addEventListener('DOMContentLoaded', function() {
 const ticketMasterApiKey = '7elxdku9GGG5k8j0Xm8KWdANDgecHMV0';
 let searchBtn = document.getElementById('search-button');
 
+/*
+Land:
+https://app.ticketmaster.eu/mfxapi/v1/event/$%7Beventid%7D?domain_ids=sweden&apikey=wRf3oq4FeoxXWIEZTHBNeexx93wdN8Vq
+
+Städer:
+https://app.ticketmaster.eu/mfxapi/v1/cities?domain_id=sweden&apikey=wRf3oq4FeoxXWIEZTHBNeexx93wdN8Vq
+*/
+
 
 searchBtn.addEventListener('click', function(event){
     retrieveSearchEventInfo()
