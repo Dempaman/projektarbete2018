77272190c832d7d88e2cc71d36e17dbecf2a2424
sabrinapalm <sabrina_palm@hotmail.com>
2018-02-28 11:36:37 +0100

    started api-stuff

--- js/events.js
+++ js/events.js
@@ -1,18 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-
+const ticketMasterApiKey = '7elxdku9GGG5k8j0Xm8KWdANDgecHMV0';
 let searchBtn = document.getElementById('search-button');
-let searchEvents = document.getElementsByClassName('search-events');
 
 
 searchBtn.addEventListener('click', function(event){
-    getSearchEvent();
+    retrieveSearchEventInfo()
+    console.log('click')
 })
 
+//ANTONS KOD, FETCH!
+function retrieveSearchEventInfo(){
+
+    fetch(`https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=${ticketMasterApiKey}`)
+    .then(function(response){
+      console.log(response);
+      return response.json();
+    })
+    .then(function(json){
+        console.log(json);
+    })
+    .catch(function(error){
+      console.log('Error');
+      console.log('Felmeddelande:',error);
+    })
 
 
-function getSearchEvent() {
-    console.log('click')
 }
 
 
@@ -36,10 +49,6 @@ function getSearchEvent() {
 
 
 
-    
-
-
-    
 
 
 
