fd395d0af694de73a2f20770b1fe713d355616d3
Dempaman <32450932+Dempaman@users.noreply.github.com>
2018-03-01 14:55:57 +0100

    menu update

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,27 @@
 //känner av om de finns en användaren
 window.addEventListener('load', function(event){
 
+  let modalWrapper = document.getElementById('modalWrapper');
+  let menuDiv = document.getElementById('menuDiv');
   let loginInMenu = document.getElementsByClassName('loginInMenu')[0];
   //Tar bort användern från localStorage
   loginInMenu.addEventListener('click', function(event){
-    localStorage.removeItem('loggedInUser');
-  });
 
-  if(localStorage.getItem('loggedInUser')){
-    loginInMenu.innerText = "logga ut";
-    console.log('THE USER ISS LOGGED IN YEAHÄ!!!!')
-  };
+    if(localStorage.getItem('loggedInUser')){
+      localStorage.removeItem('loggedInUser');
+      loginInMenu.innerText = "LOGGA IN!!!";
+    }else if(!localStorage.getItem('loggedInUser')){
+      console.log('HÄR SKA VI LOGGA IN ANVÄNDAREN');
+    }
 
+    });
+    if(localStorage.getItem('loggedInUser')){
+      loginInMenu.innerText = "LOGGA UT!!!";
+    }
+
+    menuDiv.addEventListener('click', function(event){
+      modalWrapper.className = 'hidden'
+      menuToggle.className = '';
+    })
 
 });
