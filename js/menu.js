b742b1d73c95f7eae3faafebba2f52b62586a9e0
Dempaman <32450932+Dempaman@users.noreply.github.com>
2018-03-01 23:22:14 +0100

    removes chat when you sign-out

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,25 @@
 //känner av om de finns en användaren
 window.addEventListener('load', function(event){
 
+//function som hjälper till att ta bort Elements (Tar bort alla moreMeetupInfoDiv när man loggar ut.)
+Element.prototype.remove = function() {
+  this.parentElement.removeChild(this);
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+  for(var i = this.length - 1; i >= 0; i--) {
+      if(this[i] && this[i].parentElement) {
+          this[i].parentElement.removeChild(this[i]);
+      }
+  }
+}
+
   let meetupWrapper = document.getElementById('meetupWrapper');
   let menuDiv = document.getElementById('menuDiv');
   let purple = document.getElementsByClassName('purple')[0];
   let modalWrapper = document.getElementById('modalWrapper');
   let navigation = document.getElementById('navigation');
   let loginInMenu = document.getElementsByClassName('loginInMenu')[0];
-
+  let moreMeetupInfoDiv = document.getElementsByClassName('moreMeetupInfoDiv')
   //Tar bort användern från localStorage och loggar ut från google i Firebase.
   loginInMenu.addEventListener('click', function(event){
     if(localStorage.getItem('loggedInUser')){
@@ -17,7 +29,9 @@ window.addEventListener('load', function(event){
 
       firebase.auth().signOut().then(function() {
         // Sign-out successful.
+        moreMeetupInfoDiv.remove();
         console.log('Google sign-out successful')
+
       }).catch(function(error) {
         // An error happened.
         console.log('No server response...')
