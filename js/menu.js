8d5d4fd1cba5a5697ea65f1daacae90ed2a842ce
Dempaman <32450932+Dempaman@users.noreply.github.com>
2018-03-01 12:54:48 +0100

    new cool stuff

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,16 @@
 //känner av om de finns en användaren
-if(localStorage.getItem('loggedInUser')){
-  document.getElementById('loginInMenu').innerText = "logga ut";
-};
+window.addEventListener('load', function(event){
+
+  let loginInMenu = document.getElementsByClassName('loginInMenu')[0];
+  //Tar bort användern från localStorage
+  loginInMenu.addEventListener('click', function(event){
+    localStorage.removeItem('loggedInUser');
+  });
+
+  if(localStorage.getItem('loggedInUser')){
+    loginInMenu.innerText = "logga ut";
+    console.log('THE USER ISS LOGGED IN YEAHÄ!!!!')
+  };
+
+
+});
