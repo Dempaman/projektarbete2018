44dd56d206b825af36aa13226893dee2461e9f10
Elin Jakobsson <elin-jakobsson@hotmail.com>
2018-03-02 18:07:13 +0100

    logo alternativ

--- js/profil.js
+++ js/profil.js
@@ -1,21 +1,22 @@
 
+
 window.addEventListener('load', profilFunction);
 
 function profilFunction(event) {
+
+
+  // User info text - show more/ show less
   let more = document.getElementById('user-info-more');
   let showMore = document.getElementsByClassName('show-more')[0];
   let userInfo = document.getElementsByClassName('white-cover')[0];
 
-
   more.addEventListener('click', function(event) {
       event.preventDefault();
       if (showMore.innerText === 'visa mer') {
-
           showMore.innerText = 'visa mindre';
           let upImg = document.createElement('i');
           upImg.className = "fas fa-angle-up fa-2x";
           more.children[1].parentElement.replaceChild(upImg, more.children[1]);
-
           userInfo.classList.add('transform');
           more.classList.add('move');
       } else {
@@ -29,8 +30,11 @@ function profilFunction(event) {
               more.classList.remove('move');
     }
 
-  });
+  }); // End of - User info text
 
+
+
+  // To checkout meetups or remove them - hidden nav
   let smallMenu = document.getElementsByClassName('show-hidden-nav')[0];
   let closeSmallMenu = document.getElementsByClassName('close-small-menu')[0];
   let hiddenNav = document.getElementsByClassName('hidden-nav')[0];
@@ -52,7 +56,13 @@ closeSmallMenu.addEventListener('click', function(event) {
     if (hiddenNav.className === 'hidden-nav show') {
         hiddenNav.classList.remove('show');
     }
-});
+}); // End of -To checkout meetups or remove them - hidden nav
+
+  let userEdit = document.getElementById('edit-user');
+
+  userEdit.addEventListener('click', function(event){
+    
+  });
 
 
 }
