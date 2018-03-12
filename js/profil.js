a65e45e3f3fdbe9052e6eb94095c91d6f454a78c
Elin Jakobsson <elin-jakobsson@hotmail.com>
2018-03-12 16:27:26 +0100

    profile for user and others

--- js/profil.js
+++ js/profil.js
@@ -16,48 +16,172 @@ window.addEventListener('load', profilFunction);
 
 
 
-// function getUserMeetups() {
-//
-// }
-//
-// function getUserCreatedMeetups() {
+// if (profilId) {
+//   console.log('You have clicked on a profil');
+//   if (userLog === profilId) {
+//     profileName = userLog.fullname;
+//     profileImg = userLog.avatarURL;
+//     uniqueID = userLog.uniqueID;
+//     console.log('This is your profil');
+//   } else {
+//     profileName = profilId.fullname;
+//     profileImg = profilId.avatarURL;
+//     uniqueID = profilId.uniqueID;
+//     console.log('this is ' + profilId.fullname + 'profile');
+//   }
+
+
+
+
+function profilePage() {
+  console.log(window.location.href);
+  console.log(window.location.pathname);
+  console.log('test ' + window.location.search);
+
+}
+
+// function getuserCreated() {
 //
 // }
 
 function ifUserIsTrue() {
+  // Hämta alla element som behöver informationen
   let userName = document.getElementById('user-name');
   let userImg = document.getElementById('user-img');
   let meetupCount = document.getElementById('user-joind-meetups');
   let createdCount = document.getElementById('user-created-meetups');
 
+  // variable för att spara och föra vidare data
   let data;
   let profileName;
   let profileImg;
   let uniqueID;
 
+  let meetups;
+  let userCreated;
+
+  // Är användaren inloggad
   const userLog = JSON.parse(localStorage.getItem('loggedInUser'));
-  const profilId = 0;
+
+  // användarens sid ID querystring
+  const pageUserId = window.location.href.split('?sid=')[1];
+  console.log(pageUserId);
+
+  if (pageUserId && userLog) {
+
+    db.ref('/users/').on('value', function(snapshot){
+      let allUsers = snapshot.val();
+
+      for (let x in allUsers) {
+
+        if (allUsers[x].sid === pageUserId) {
+
+          console.log(allUsers[x]);
+
+            userName.innerText = allUsers[x].fullname;
+            userImg.src = allUsers[x].avatarURL;
+            uniqueID = allUsers[x].uniqueID;
+
+            userCreated = allUsers[x].createdMeetups;
+            meetups = allUsers[x].meetups;
+            console.log(meetups);
+
+            if (userCreated) {
+              let counter = 0;
+              for (let obj in userCreated) {
+                counter += Object.keys(userCreated[obj]).length;
+                //let currentEvent = eventObjects[obj];
+
+                // Lägg till html kod som ska loppa igenom alla meetups
+
+                console.log('eventObj ', obj);
+                console.log('eventObjects ', Object.keys(userCreated[obj]).length);
+              }
+                createdCount.innerText = counter;
+            }
+
+            if (meetups) {
+              let secondCounter = 0;
+              for (let obj in meetups) {
+                secondCounter += Object.keys(meetups[obj]).length;
+                //let currentEvent = eventObjects[obj];
+
+                // Lägg till html kod som ska loppa igenom alla meetups
+
+                console.log('meetups ', obj);
+                console.log('meetups ', Object.keys(meetups[obj]).length);
+              }
+                meetupCount.innerText = secondCounter;
+            }
+
+        }else {
+          console.log('not user');
+        }
+      }
+
+
+    });
+
+  }else if (userLog ) {
+
+    userName.innerText = userLog.fullname;
+    userImg.src = userLog.avatarURL;
+    uniqueID = userLog.uniqueID;
+
+    // Hämta alla skapade Meetups
+    db.ref('users/' + uniqueID + '/createdMeetups').on('value', function(snapshot) {
+      // child_added
+      let eventObjects = snapshot.val();
+      let eventID = snapshot.key;
+
+      if (eventObjects) {
+        let counter = 0;
+        for (let obj in eventObjects) {
+          counter += Object.keys(eventObjects[obj]).length;
+          //let currentEvent = eventObjects[obj];
+
+          // Lägg till html kod som ska loppa igenom alla meetups
+
+          console.log('eventObj ', obj);
+          console.log('eventObjects ', Object.keys(eventObjects[obj]).length);
+        }
+          createdCount.innerText = counter;
+      }
+
+    });
+
+
+  }else {
+    console.log('Sorry! No Web Storage support');
+    //let indexPage = '/index.html';
+    //let testLocalHost = window.location.protocol + '//' + window.location.hostname + ':8000' + indexPage;
+    //let relocate = window.location.href
+    //console.log(window.location.href);
+    //console.log(testLocalHost);
+    window.location.assign('index.html'); // kan komma att ändras i framtiden
+
+  }
+
+
+
+
+
+
+
+
+
+  /*console.log('pageUser ', pageUserId);
+  console.log('pageUser is a ' + typeof(pageUserId));
+
+
 
   if (userLog) {
 
     profileName = userLog.fullname;
     profileImg = userLog.avatarURL;
     uniqueID = userLog.uniqueID;
-    if (profilId) {
-      console.log('You have clicked on a profil');
-      if (userLog === profilId) {
-        profileName = userLog.fullname;
-        profileImg = userLog.avatarURL;
-        uniqueID = userLog.uniqueID;
-        console.log('This is your profil');
-      } else {
-        profileName = profilId.fullname;
-        profileImg = profilId.avatarURL;
-        uniqueID = profilId.uniqueID;
-        console.log('this is ' + profilId.fullname + 'profile');
-      }
 
-    }
+
 
     userName.innerText = profileName;
     userImg.src = profileImg;
@@ -120,12 +244,13 @@ function ifUserIsTrue() {
     //console.log(window.location.href);
     //console.log(testLocalHost);
     window.location.assign('index.html'); // kan komma att ändras i framtiden
-  }
+  }*/
 
 }
 
 function profilFunction(event) {
 
+  profilePage();
   ifUserIsTrue();
 
 
@@ -189,18 +314,18 @@ function profilFunction(event) {
     });
   } // End of -To checkout meetups or remove them - hidden nav
 
-//   let userEdit = document.getElementById('tellMeMore');
-// console.log(userEdit);
-//   userEdit.addEventListener('click', function(event) {
-//     console.log(event.type);
-//
-//     if (document.getElementsByClassName('user-story').style.display === 'none') {
-//       document.getElementsByClassName('user-story').style.display = 'block';
-//     }  else {
-//       document.getElementsByClassName('user-story').style.display = 'none';
-//     }
-//
-//   });
+  let userEdit = document.getElementById('tellMeMore');
+console.log('userEdit ' + userEdit);
+  userEdit.addEventListener('click', function(event) {
+    console.log(event.type);
+
+    if (document.getElementsByClassName('user-story')[0].style.display === 'none') {
+      document.getElementsByClassName('user-story')[0].style.display = 'block';
+    }else {
+      document.getElementsByClassName('user-story')[0].style.display = 'none';
+    }
+
+  });
 
 
 }
